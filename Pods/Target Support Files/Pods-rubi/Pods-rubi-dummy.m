a88137042ac213d5f57db73adc397c2bcbfb105c
tiking76 <716yosiki1999@gmail.com>
2020-05-18 09:12:26 +0900

    create project

---
+++ Pods/Target Support Files/Pods-rubi/Pods-rubi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rubi : NSObject
+@end
+@implementation PodsDummy_Pods_rubi
+@end
---
+++ rubi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rubi
+//
+//  Created by 舘佳紀 on 2020/05/18.
+//  Copyright © 2020 yoshiki Tachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
