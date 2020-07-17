a08c57042b2a0aaae70ec18f1c3da4e5215668f9
tiking76 <716yosiki1999@gmail.com>
2020-07-17 19:30:52 +0900

    完成

--- rubi/ResultViewController.swift
+++ rubi/ResultViewController.swift
@@ -9,22 +9,49 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
-    var argArray : [String]!
     
-    @IBOutlet weak var InputtedTextField: UITextView!
+    var context: String = ""
+    var resText: String = ""
     
-    @IBOutlet weak var ResultTextField: UITextView!
+    @IBOutlet weak var inputtedTextField: UITextView!
+    
+    @IBOutlet weak var resultTextField: UITextView!
+    
+    @IBOutlet weak var arrowLabel: UILabel!
+    
+    @IBOutlet weak var backHomeButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        InputtedTextField.text = argArray[0]
-        print(InputtedTextField.text)
-        ResultTextField.text = argArray[1]
-        print(ResultTextField.text)
+        configureGradientLayer()
+        configUI()
+        inputtedTextField.text = context
+        resultTextField.text = resText
+        
     }
     
+    @objc func backHome() {
+        dismiss(animated: true, completion: nil)
+        inputtedTextField.text = ""
+        resultTextField.text = ""
+    }
     
+    func configUI() {
+        configButton()
+        configTextField()
+        view.addSubview(arrowLabel)
+    }
+    
+    func configButton() {
+        view.addSubview(backHomeButton)
+        backHomeButton.setTitle("Home", for: .normal)
+        backHomeButton.addTarget(self, action: #selector(backHome), for: .touchUpInside)
+    }
+    
+    func configTextField() {
+        view.addSubview(inputtedTextField)
+        view.addSubview(resultTextField)
+    }
     
 }
